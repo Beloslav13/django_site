d8afcaf04fb03aaf3db696c19c02d99c5ae06084
beloslav <velislav1329@gmail.com>
2020-03-26 21:17:49 +0300

    добавил статик файлы, создал приложение

--- voit_it/voit_it/settings.py
+++ voit_it/voit_it/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogengine.apps.BlogengineConfig'
 ]
 
 MIDDLEWARE = [
@@ -130,4 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
