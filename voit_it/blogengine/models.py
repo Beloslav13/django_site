f51698c27b06650c462f33c45caf57a3b6245d5e
beloslav <velislav1329@gmail.com>
2020-03-28 11:59:07 +0300

    связал автора поста с пользователем в системе. добавил русские названия для полей

--- voit_it/blogengine/models.py
+++ voit_it/blogengine/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -7,12 +8,17 @@ from django.utils import timezone
 
 class Post(models.Model):
     """Модель поста."""
-    author = models.CharField(max_length=150)
-    title = models.CharField(max_length=150)
-    text = models.TextField()
-    image = models.ImageField(upload_to='uploads', blank=True)
-    create_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Детальное описание поста')
+    image = models.ImageField(upload_to='uploads', blank=True, verbose_name='Изображение')
+    create_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    published_date = models.DateTimeField(blank=True, null=True, verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-published_date', 'title']
 
     def published(self):
         """Метод публикации поста"""
