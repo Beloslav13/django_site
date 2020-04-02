324bfda5e5371a680f5008140b4e60dcbf66428e
beloslav <velislav1329@gmail.com>
2020-04-02 13:09:21 +0300

    изменил представление в админке для модели Пост

--- voit_it/blogengine/admin.py
+++ voit_it/blogengine/admin.py
@@ -6,6 +6,10 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     """Класс для автозаполениня slug из модели Post по полю title."""
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('author', 'title', 'text', 'published_date')
+    list_filter = ('published_date',)
+    list_display_links = ('author', 'title',)
+    search_fields = ["title", "text"]
 
 
 admin.site.register(Post, PostAdmin)
