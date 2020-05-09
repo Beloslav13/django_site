90b82aa8682b1e2ff22ad21a63f3aedb5f1d4dd4
beloslav <velislav1329@gmail.com>
2020-05-09 15:00:33 +0300

    реализовал тег последних постов, убрал копипаст.

--- voit_it/blogengine/templatetags/blogengine_tags.py
+++ voit_it/blogengine/templatetags/blogengine_tags.py
@@ -4,7 +4,14 @@ from django.contrib.sites.shortcuts import get_current_site
 from classytags.core import Tag, Options
 from classytags.arguments import Argument
 
+from blogengine.models.posts import Post
+
 register = template.Library()
+
+
+@register.filter(name='split')
+def split(value, sep):
+    return value.split(sep)
 
 
 class SiteLogo(Tag):
@@ -35,9 +42,26 @@ class SiteLogo(Tag):
             return ''
 
 
-@register.filter(name='split')
-def split(value, sep):
-    return value.split(sep)
-
-
 register.tag(SiteLogo)
+
+
+class LastPosts(Tag):
+    name = 'last_posts'
+
+    options = Options(
+        'as',
+        Argument('varname', required=False, resolve=False)
+    )
+
+    def render_tag(self, context, varname, name=name):
+        last_posts = Post.objects.all()[:3]
+        if varname:
+            context[varname] = last_posts
+            return ''
+        else:
+            context[name] = last_posts
+            return ''
+
+
+register.tag(LastPosts)
+
