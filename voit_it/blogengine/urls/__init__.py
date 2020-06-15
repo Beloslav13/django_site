af479d8681b751f590cca6601561f406b2dd4b3d
beloslav <velislav1329@gmail.com>
2020-06-15 13:23:17 +0300

    fix styles posts

--- voit_it/blogengine/urls/__init__.py
+++ voit_it/blogengine/urls/__init__.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogengine.urls.blogengine')),
+    path('new/', TemplateView.as_view(template_name='new.html'), name='new'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('api/', include('blogengine.urls.rest_api')),
 
